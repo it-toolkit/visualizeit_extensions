6812192f70b358b68be4154b5f87c3c83b65f2bd
Fernando Mansilla <fernando.e.mansilla@gmail.com>
2024-02-25 22:25:08 -0300

    feat: Initial implementation of logging lib

---
+++ lib/logging.dart
@@ -0,0 +1,79 @@
+
+import 'package:flutter/foundation.dart';
+
+abstract class LogEventHandler {
+  void handle(LogLevel level, String category, String Function() msgBuilder, {Error? error, StackTrace? stackTrace});
+}
+
+class Logging implements LogEventHandler {
+  static final Logging _instance = Logging._internal();
+
+  Map<String, Logger> _loggers = {};
+
+  factory Logging() {
+    return _instance;
+  }
+
+  Logging._internal() {
+    // initialization logic
+  }
+
+  Logger? _getLogger(String category) => _loggers[category];
+
+  void _registerLogger(Logger logger) {
+    if (!_loggers.containsKey(logger.category)) {
+      final loggers = Map<String,Logger>.from(_loggers);
+      loggers[logger.category] = logger;
+      _loggers = loggers;
+    }
+  }
+
+  @override
+  void handle(LogLevel level, String category, String Function() msgBuilder, {Error? error, StackTrace? stackTrace}) {
+    if (kDebugMode) {
+      debugPrint("${DateTime.now()} ${level.name.toUpperCase()} [$category] - ${msgBuilder()}");
+    }
+  }
+}
+
+enum LogLevel {
+  error, warn, info, debug, trace;
+}
+
+class Logger {
+  String category;
+  late LogEventHandler _logEventHandler;
+
+  factory Logger(String category) {
+    final logging = Logging();
+    var logger = logging._getLogger(category);
+    if (logger != null) return logger;
+
+    logger = Logger._internal(category);
+    logger._logEventHandler = logging;
+    logging._registerLogger(logger);
+    return logger;
+  }
+
+  Logger._internal(this.category);
+
+  void trace(String Function() msgBuilder){
+    _logEventHandler.handle(LogLevel.trace, category, msgBuilder);
+  }
+
+  void debug(String Function() msgBuilder) {
+    _logEventHandler.handle(LogLevel.debug, category, msgBuilder);
+  }
+
+  void info(String Function() msgBuilder) {
+    _logEventHandler.handle(LogLevel.info, category, msgBuilder);
+  }
+
+  void warn(String Function() msgBuilder) {
+    _logEventHandler.handle(LogLevel.warn, category, msgBuilder);
+  }
+
+  void error(String Function() msgBuilder, {Error? error, StackTrace? stackTrace}){
+    _logEventHandler.handle(LogLevel.trace, category, msgBuilder, error: error, stackTrace: stackTrace);
+  }
+}

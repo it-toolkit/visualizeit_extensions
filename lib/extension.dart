0599af67dc1e1baa5cdba0ef52b4dbc7296eb354
Fernando Mansilla <fernando.e.mansilla@gmail.com>
2024-05-04 12:40:53 -0300

    feat: Support async extension build and add markdown docs references

--- lib/extension.dart
+++ lib/extension.dart
@@ -3,15 +3,24 @@ import 'package:visualizeit_extensions/scripting.dart';
 import 'package:visualizeit_extensions/visualizer.dart';
 
 typedef ExtensionId = String;
+typedef LanguageCode = String;
+
+abstract class LanguageCodes {
+  /// English language code
+  static const en = "en";
+  /// Spanish language code
+  static const es = "es";
+}
 
 class Extension {
   ExtensionId extensionId;
   ScriptingExtension scripting;
   VisualizerExtension visualizer;
+  Map<LanguageCode, String> markdownDocs;
 
-  Extension(this.extensionId, this.scripting, this.visualizer);
+  Extension(this.extensionId, this.scripting, this.visualizer, this.markdownDocs);
 }
 
 abstract class ExtensionBuilder {
-  Extension build();
+  Future<Extension> build();
 }

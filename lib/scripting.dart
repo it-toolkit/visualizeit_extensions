579cef840ce0cfbf7245d01d5502e4ec4b8a2b2d
Fernando Mansilla <fernando.e.mansilla@gmail.com>
2024-05-11 16:59:44 -0300

    feat: Support named command args

--- lib/scripting.dart
+++ lib/scripting.dart
@@ -7,7 +7,8 @@ enum ArgType {
   string,
   int,
   double,
-  boolean
+  boolean,
+  stringArray
 }
 
 class CommandArgDef {
@@ -15,7 +16,26 @@ class CommandArgDef {
   ArgType type;
 
   CommandArgDef(this.name, this.type);
+
+  dynamic convert(dynamic value) {
+    return switch (type) {
+      ArgType.string => value is String ? value : value.toString(),
+      ArgType.int => value is num ? value.toInt() : int.tryParse(value.toString()).throwIfNull(Exception("Cannot convert '$value' to $type")),
+      ArgType.double => value is num ? value.toDouble() : double.tryParse(value.toString()).throwIfNull(Exception("Cannot convert '$value' to $type")),
+      ArgType.boolean => value is bool ? value : bool.tryParse(value.toString(), caseSensitive: false).throwIfNull(Exception("Cannot convert '$value' to $type")),
+      ArgType.stringArray => value is List<String> ? value : (value is List ? value.map((e) => e.toString()) : null).throwIfNull(Exception("Cannot convert '$value' to $type"))
+    };
+  }
 }
+
+extension _DynamicExtension on dynamic {
+  dynamic throwIfNull(Exception exception) {
+    if(this == null) throw exception;
+
+    return this;
+  }
+}
+
 
 class CommandDefinition {
   ExtensionId extensionId;
@@ -23,11 +43,81 @@ class CommandDefinition {
   List<CommandArgDef> args;
 
   CommandDefinition(this.extensionId, this.name, this.args);
+
+  dynamic getArg({required String name, required RawCommand from}) {
+    final argPosition = _getArgPositionByName(name);
+    final argType = args[argPosition];
+
+    if (from is RawCommandWithPositionalArgs) {
+      return argType.convert(from.getArg(argPosition));
+    } else if (from is RawCommandWithNameArgs) {
+      return argType.convert(from.getArg(name));
+    }
+
+    throw Exception("Unexpected raw command type: $from");
+  }
+
+  int _getArgPositionByName(String name) {
+    var index = args.indexWhere((arg) => arg.name == name);
+    if(index < 0) throw Exception("Unknown argument name: $name");
+    return index;
+  }
 }
 
 abstract class ScriptingExtension {
   List<CommandDefinition> getAllCommandDefinitions();
 
   ///Returns a valid command if a compatible [CommandDefinition] is found or null otherwise
-  Command? buildCommand(String rawCommand);
+  Command? buildCommand(RawCommand rawCommand);
+}
+
+abstract class RawCommand {
+
+  final String name;
+
+  int argsLength();
+
+  const RawCommand._(this.name); // Private constructor
+
+  factory RawCommand.literal(String name) = RawLiteralCommand;
+  factory RawCommand.withPositionalArgs(String name, List<dynamic> args) = RawCommandWithPositionalArgs;
+  factory RawCommand.withNamedArgs(String name, Map<String, dynamic> namedArgs) = RawCommandWithNameArgs;
+}
+
+class RawLiteralCommand extends RawCommand {
+  RawLiteralCommand(super.name) : super._(); // Private constructor
+
+  @override
+  int argsLength() => 0;
+
+  @override
+  String toString() => 'RawLiteralCommand {name=$name}';
+}
+
+class RawCommandWithPositionalArgs extends RawCommand {
+  final List<dynamic> args;
+
+  RawCommandWithPositionalArgs(super.name, this.args) : super._(); // Private constructor
+
+  @override
+  int argsLength() => args.length;
+
+  dynamic getArg(int index) => args[index];
+
+  @override
+  String toString() => 'RawLiteralCommand {name=$name, args=$args}';
+}
+
+class RawCommandWithNameArgs extends RawCommand {
+  final Map<String, dynamic> namedArgs;
+
+  RawCommandWithNameArgs(super.name, this.namedArgs) : super._(); // Private constructor
+
+  @override
+  int argsLength() => namedArgs.length;
+
+  dynamic getArg(String name) => namedArgs[name];
+
+  @override
+  String toString() => 'RawLiteralCommand {name=$name, args=$namedArgs}';
 }
---
+++ test/scripting_test.dart
@@ -0,0 +1,55 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:visualizeit_extensions/scripting.dart';
+
+void main() {
+  final commandDefinition = CommandDefinition(
+      "an-extension-id",
+      "a-command-name", [
+        CommandArgDef("param1", ArgType.string),
+        CommandArgDef("param2", ArgType.int),
+        CommandArgDef("param3", ArgType.double),
+        CommandArgDef("param4", ArgType.boolean),
+        CommandArgDef("param5", ArgType.stringArray),
+      ],
+  );
+
+  final rawLiteralCommand = RawCommand.literal("a-command-name");
+
+  final rawCommandWithPositionalArgs = RawCommand.withPositionalArgs("a-command-name", ["text", 1, 2.3, true, ["a", "2"]]);
+  final rawCommandWithPositionalCastableArgs = RawCommand.withPositionalArgs("a-command-name", ["text", "1", "2.3", "true", ["a", 2]]);
+  final rawCommandWithNamedArgs = RawCommand.withNamedArgs("a-command-name", {"param1": "text", "param2": 1, "param3": 2.3, "param4": true, "param5": ["a", "2"]});
+  final rawCommandWithNamedCastableArgs = RawCommand.withNamedArgs("a-command-name", {"param1": "text", "param2": "1", "param3": "2.3", "param4": "true", "param5": ["a", 2]});
+
+  final rawCommandWithArgs = [rawCommandWithPositionalArgs, rawCommandWithPositionalCastableArgs, rawCommandWithNamedArgs, rawCommandWithNamedCastableArgs];
+
+  final rawCommandWithInvalidPositionalArgs = RawCommand.withPositionalArgs("a-command-name", [null, "a", "b", "c", {"a": "2"}]);
+
+  test('throw error when try to get an arg for a command without args', () {
+    expect(() => commandDefinition.getArg(name: "param1", from: rawLiteralCommand), throwsA(isA<Exception>()));
+  });
+
+  test('throw error when try to get an unknown arg for a command', () {
+    for (var rawCommand in rawCommandWithArgs) {
+      expect(() => commandDefinition.getArg(name: "unknownParam", from: rawCommand), throwsA(isA<Exception>()));
+    }
+  });
+
+  test('throw error when try to get an arg with invalid type', () {
+    expect(commandDefinition.getArg(name: "param1", from: rawCommandWithInvalidPositionalArgs), equals("null"));
+
+    expect(() => commandDefinition.getArg(name: "param2", from: rawCommandWithInvalidPositionalArgs), throwsA(isA<Exception>()));
+    expect(() => commandDefinition.getArg(name: "param3", from: rawCommandWithInvalidPositionalArgs), throwsA(isA<Exception>()));
+    expect(() => commandDefinition.getArg(name: "param4", from: rawCommandWithInvalidPositionalArgs), throwsA(isA<Exception>()));
+    expect(() => commandDefinition.getArg(name: "param5", from: rawCommandWithInvalidPositionalArgs), throwsA(isA<Exception>()));
+  });
+
+  test('get proper casted arg values from raw commands', () {
+    for (var rawCommand in rawCommandWithArgs) {
+      expect(commandDefinition.getArg(name: "param1", from: rawCommand), equals("text"));
+      expect(commandDefinition.getArg(name: "param2", from: rawCommand), equals(1));
+      expect(commandDefinition.getArg(name: "param3", from: rawCommand), equals(2.3));
+      expect(commandDefinition.getArg(name: "param4", from: rawCommand), equals(true));
+      expect(commandDefinition.getArg(name: "param5", from: rawCommand), equals(["a", "2"]));
+    }
+  });
+}
